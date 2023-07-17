b121ec7fed6e4ff0308b72d7f7edb546f8d5134e
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-07-17 12:01:58 +0100

    🔥 Enums

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
     println!("Hello, world!");
+
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+ 
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Server { addr }
+    }
+
+    fn run(self) {
+        print!("Listening on {}", self.addr)
+    }
+}
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
 }
