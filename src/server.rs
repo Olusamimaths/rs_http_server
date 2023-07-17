844028091889c630c8c56a2123ed9376a3407cdc
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-07-17 12:42:20 +0100

    🚑 Enuming Matching Imperative

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -8,6 +10,17 @@ impl Server {
     }
 
     pub fn run(self) {
-        print!("Listening on {}", self.addr)
+        print!("Listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            let res = listener.accept();
+            if res.is_err() {
+                continue;
+            }
+
+            let (stream, addr) = res.unwrap();
+        }
     }
 }
