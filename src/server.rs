dc65a299e015c6d3456d5a7ccb2845dab4e5cd8a
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-07-20 13:33:09 +0100

    🔥 Responses

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
-use std::io::Read;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
+use std::io::{Read, Write};
 use std::net::TcpListener;
-use crate::http::Request;
 
 pub struct Server {
     addr: String,
@@ -14,30 +14,35 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.addr);
-
         let listener = TcpListener::bind(&self.addr).unwrap();
-
         loop {
             match listener.accept() {
-                Err(error) => println!("Failed to establish connection: {}", error),
-
                 Ok((mut stream, _)) => {
-                    let mut buffer = [0; 1024];
-                    match stream.read(&mut buffer) {
-                        Ok(_) => {
-                            println!("Receieved request: {}", String::from_utf8_lossy(&buffer));
-
-                            match Request::try_from(&buffer[..]) {
-                                Ok(request) => {
-                                    dbg!(request);
-                                },
-                                Err(e) => print!("Failed to parse a request: {}", e),
-                            }
-                        }
-                        Err(e) => println!("Failed to read from connection: {}", e),
-                    }
+                    handle_ok_request(stream);
                 }
+                Err(error) => println!("Failed to establish connection: {}", error),
             }
         }
+    }
+}
+
+fn handle_ok_request(mut stream: std::net::TcpStream) {
+    let mut buffer = [0; 1024];
+    match stream.read(&mut buffer) {
+        Ok(_) => {
+            println!("Receieved request: {}", String::from_utf8_lossy(&buffer));
+
+            match Request::try_from(&buffer[..]) {
+                Ok(request) => {
+                    // dbg!(request);
+                    let response = Response::new(StatusCode::Ok, Some(
+                        "<h1>Welcome to My Rust Server</h1>".to_string()
+                    ));
+                    write!(stream, "{}", response);
+                }
+                Err(e) => print!("Failed to parse a request: {}", e),
+            }
+        }
+        Err(e) => println!("Failed to read from connection: {}", e),
     }
 }
