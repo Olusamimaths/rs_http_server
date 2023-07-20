e490f1d1751c112e54c5b1bfddaee68fa322afb6
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-07-20 15:26:59 +0100

    🔥 Handling routes

--- src/request_handler.rs
+++ src/request_handler.rs
@@ -1,10 +1,17 @@
-use super::http::{Request, Response, StatusCode};
+use super::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
 
 pub struct RequestHandler;
 
 impl Handler for RequestHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>Welcome again </h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>HomePage</h1>".to_string())),
+                "/posts" => Response::new(StatusCode::Ok, Some("<h1>This is our page</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None)
+            }
+            _ => Response::new(StatusCode::NotFound, None)
+        }
     }
 }
