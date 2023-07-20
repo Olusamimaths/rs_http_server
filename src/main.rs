86af7467ad721b4358bbd7d0dddbe767a8a5ed60
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-07-20 15:39:06 +0100

    ✨ Env setup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 
+use std::env;
 use server::Server;
 use request_handler::RequestHandler;
 
@@ -8,7 +9,8 @@ mod http;
 mod request_handler;
 
 fn main() {
+    let public_path = env::var("PUBLIC_PATH").unwrap();
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(RequestHandler);
+    server.run(RequestHandler::new(public_path));
 }
 
--- src/request_handler.rs
+++ src/request_handler.rs
@@ -1,17 +1,28 @@
-use super::http::{Request, Response, StatusCode, Method};
+use super::http::{Method, Request, Response, StatusCode};
 use super::server::Handler;
 
-pub struct RequestHandler;
+pub struct RequestHandler {
+    public_path: String,
+}
+
+impl RequestHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+}
 
 impl Handler for RequestHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, Some("<h1>HomePage</h1>".to_string())),
-                "/posts" => Response::new(StatusCode::Ok, Some("<h1>This is our page</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, None)
-            }
-            _ => Response::new(StatusCode::NotFound, None)
+                "/posts" => Response::new(
+                    StatusCode::Ok,
+                    Some("<h1>This is our page</h1>".to_string()),
+                ),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+            _ => Response::new(StatusCode::NotFound, None),
         }
     }
 }
