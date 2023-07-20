6d9c8d3be1a03176a36901baeb068d31631abb58
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-07-20 15:55:29 +0100

    ⚡️ Serving Html

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ mod http;
 mod request_handler;
 
 fn main() {
-    let public_path = env::var("PUBLIC_PATH").unwrap();
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run(RequestHandler::new(public_path));
 }
--- src/request_handler.rs
+++ src/request_handler.rs
@@ -1,5 +1,6 @@
 use super::http::{Method, Request, Response, StatusCode};
 use super::server::Handler;
+use std::fs;
 
 pub struct RequestHandler {
     public_path: String,
@@ -9,16 +10,21 @@ impl RequestHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
     }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+    }
 }
 
 impl Handler for RequestHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>HomePage</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/posts" => Response::new(
                     StatusCode::Ok,
-                    Some("<h1>This is our page</h1>".to_string()),
+                    self.read_file("post.html"),
                 ),
                 _ => Response::new(StatusCode::NotFound, None),
             },
